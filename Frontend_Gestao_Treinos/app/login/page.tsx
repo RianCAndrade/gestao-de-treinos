ddd95da8e24710a470bc2314d89c26a0a2698a93
Rian Andrade <riannewsouls@gmail.com>
2026-06-19 13:45:25 -0300

    implementado o sentry no nextjs

--- Frontend_Gestao_Treinos/app/login/page.tsx
+++ Frontend_Gestao_Treinos/app/login/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import * as Sentry from "@sentry/nextjs";
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
